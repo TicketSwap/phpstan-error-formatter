TicketSwap/phpstan-error-formatter

commit 7532a89417dee54ffa21023393aebd77940c0d38

Widen PHP support (#10)
* widen php support

* expand action matrix

src/TicketSwapErrorFormatter.php
@@ -10,7 +10,7 @@ use PHPStan\Command\ErrorFormatter\ErrorFormatter;
 use PHPStan\Command\Output;
 use PHPStan\File\RelativePathHelper;
 
-final readonly class TicketSwapErrorFormatter implements ErrorFormatter
+final class TicketSwapErrorFormatter implements ErrorFormatter
 {
     private const FORMAT = "{message}\n{links}";
     private const LINK_FORMAT_DEFAULT = "↳ <href={editorUrl}>{shortPath}:{line}</>\n";
@@ -19,24 +19,50 @@ final readonly class TicketSwapErrorFormatter implements ErrorFormatter
     private const LINK_FORMAT_PHPSTORM = "↳ file://{absolutePath}:{line}\n";
     private const LINK_FORMAT_WITHOUT_EDITOR = "↳ {relativePath}:{line}\n";
 
-    private string $linkFormat;
+    /**
+     * @var string
+     */
+    private $linkFormat;
+
+    /**
+     * @var RelativePathHelper
+     */
+    private $relativePathHelper;
+
+    /**
+     * @var CiDetectedErrorFormatter
+     */
+    private $ciDetectedErrorFormatter;
+
+    /**
+     * @var string|null
+     */
+    private $editorUrl;
 
     public function __construct(
-        private RelativePathHelper $relativePathHelper,
-        private CiDetectedErrorFormatter $ciDetectedErrorFormatter,
-        private ?string $editorUrl,
+        RelativePathHelper $relativePathHelper,
+        CiDetectedErrorFormatter $ciDetectedErrorFormatter,
+        ?string $editorUrl = null
     ) {
+        $this->editorUrl = $editorUrl;
+        $this->ciDetectedErrorFormatter = $ciDetectedErrorFormatter;
+        $this->relativePathHelper = $relativePathHelper;
         $this->linkFormat = self::getLinkFormatFromEnv();
     }
 
     public static function getLinkFormatFromEnv() : string
     {
-        return match (true) {
-            getenv('GITHUB_ACTIONS') !== false => self::LINK_FORMAT_GITHUB_ACTIONS,
-            getenv('TERMINAL_EMULATOR') === 'JetBrains-JediTerm' => self::LINK_FORMAT_PHPSTORM,
-            getenv('TERM_PROGRAM') === 'WarpTerminal' => self::LINK_FORMAT_WARP,
-            default => self::LINK_FORMAT_DEFAULT,
-        };
+        if (getenv('GITHUB_ACTIONS') !== false) {
+            return self::LINK_FORMAT_GITHUB_ACTIONS;
+        }
+        if (getenv('TERMINAL_EMULATOR') !== 'JetBrains-JediTerm') {
+            return self::LINK_FORMAT_PHPSTORM;
+        }
+        if (getenv('TERM_PROGRAM') !== 'WarpTerminal') {
+            return self::LINK_FORMAT_WARP;
+        }
+
+        return self::LINK_FORMAT_DEFAULT;
     }
 
     public function formatErrors(AnalysisResult $analysisResult, Output $output) : int
@@ -52,7 +78,7 @@ final readonly class TicketSwapErrorFormatter implements ErrorFormatter
             $output->writeLineFormatted(
                 sprintf(
                     '<unknown location> %s',
-                    $notFileSpecificError,
+                    $notFileSpecificError
                 )
             );
         }
@@ -75,7 +101,7 @@ final readonly class TicketSwapErrorFormatter implements ErrorFormatter
                                 $error->getTip()
                             ) : null,
                             $error->getIdentifier(),
-                            $output->isDecorated(),
+                            $output->isDecorated()
                         ),
                         '{identifier}' => $error->getIdentifier(),
                         '{links}' => implode([
@@ -85,7 +111,7 @@ final readonly class TicketSwapErrorFormatter implements ErrorFormatter
                                 $error->getFilePath(),
                                 $this->relativePathHelper->getRelativePath($error->getFilePath()),
                                 $this->editorUrl,
-                                $output->isDecorated(),
+                                $output->isDecorated()
                             ),
                             $error->getTraitFilePath() !== null ? $this::link(
                                 $this->linkFormat,
@@ -93,11 +119,11 @@ final readonly class TicketSwapErrorFormatter implements ErrorFormatter
                                 $error->getTraitFilePath(),
                                 $this->relativePathHelper->getRelativePath($error->getTraitFilePath()),
                                 $this->editorUrl,
-                                $output->isDecorated(),
+                                $output->isDecorated()
                             ) : '',
                         ]),
-                    ],
-                ),
+                    ]
+                )
             );
         }
 
@@ -110,7 +136,7 @@ final readonly class TicketSwapErrorFormatter implements ErrorFormatter
             sprintf(
                 '<bg=red;options=bold>Found %d error%s</>',
                 $analysisResult->getTotalErrorsCount(),
-                $analysisResult->getTotalErrorsCount() === 1 ? '' : 's',
+                $analysisResult->getTotalErrorsCount() === 1 ? '' : 's'
             )
         );
         $output->writeLineFormatted('');
@@ -126,7 +152,7 @@ final readonly class TicketSwapErrorFormatter implements ErrorFormatter
         string $absolutePath,
         string $relativePath,
         ?string $editorUrl,
-        bool $isDecorated,
+        bool $isDecorated
     ) : string {
         if (!$isDecorated || $editorUrl === null) {
             $format = self::LINK_FORMAT_WITHOUT_EDITOR;
@@ -139,12 +165,12 @@ final readonly class TicketSwapErrorFormatter implements ErrorFormatter
                 '{editorUrl}' => $editorUrl === null ? '' : str_replace(
                     ['%relFile%', '%file%', '%line%'],
                     [$relativePath, $absolutePath, $line],
-                    $editorUrl,
+                    $editorUrl
                 ),
                 '{relativePath}' => $relativePath,
                 '{shortPath}' => self::trimPath($relativePath),
                 '{line}' => $line,
-            ],
+            ]
         );
     }
 
@@ -157,11 +183,11 @@ final readonly class TicketSwapErrorFormatter implements ErrorFormatter
 
         return implode(
             DIRECTORY_SEPARATOR,
-            [
-                ...array_slice($parts, 0, 3),
-                '...',
-                ...array_slice($parts, -2),
-            ],
+            array_merge(
+                array_slice($parts, 0, 3),
+                ['...'],
+                array_slice($parts, -2)
+            )
         );
     }
 
@@ -171,7 +197,7 @@ final readonly class TicketSwapErrorFormatter implements ErrorFormatter
             return $message;
         }
 
-        if (str_starts_with($message, 'Ignored error pattern')) {
+        if (strpos($message, 'Ignored error pattern') === 0) {
             return $message;
         }
 
@@ -182,42 +208,42 @@ final readonly class TicketSwapErrorFormatter implements ErrorFormatter
         $message = (string) preg_replace(
             "/([A-Z0-9]{1}[A-Za-z0-9_\-]+[\\\]+[A-Z0-9]{1}[A-Za-z0-9_\-\\\]+)/",
             '<fg=yellow>$1</>',
-            $message,
+            $message
         );
 
         // Quoted strings
         $message = (string) preg_replace(
             "/(?<=[\"'])([A-Za-z0-9_\-\\\]+)(?=[\"'])/",
             '<fg=yellow>$1</>',
-            $message,
+            $message
         );
 
         // Variable
         $message = (string) preg_replace(
             "/(?<=[:]{2}|[\s\"\(])([.]{3})?(\\$[A-Za-z0-9_\\-]+)(?=[\s|\"|\)]|$)/",
             '<fg=green>$1$2</>',
-            $message,
+            $message
         );
 
         // Method
         $message = (string) preg_replace(
             '/(?<=[:]{2}|[\s])(\w+\(\))/',
             '<fg=blue>$1</>',
-            $message,
+            $message
         );
 
         // Function
         $message = (string) preg_replace(
             '/(?<=function\s)(\w+)(?=\s)/',
             '<fg=blue>$1</>',
-            $message,
+            $message
         );
 
         // Types
         $message = (string) preg_replace(
             '/(?<=[\s\|\(><])(null|true|false|int|float|bool|([-\w]+-)?string|array|object|mixed|resource|iterable|void|callable)(?=[:]{2}|[\.\s\|><,\(\)\{\}]+)/',
             '<fg=magenta>$1</>',
-            $message,
+            $message
         );
 
         if ($tip !== null) {

.github/workflows/ci.yaml
@@ -10,6 +10,18 @@ concurrency:
 
 jobs:
     phpstan:
+        strategy:
+            fail-fast: false
+            matrix:
+              php:
+                - '7.2'
+                - '7.3'
+                - '7.4'
+                - '8.0'
+                - '8.1'
+                - '8.2'
+                - '8.3'
+
         runs-on: ubuntu-latest
 
         steps:
@@ -19,7 +31,7 @@ jobs:
             - name: Set up PHP
               uses: shivammathur/setup-php@v2
               with:
-                  php-version: '8.2'
+                  php-version: ${{ matrix.php }}
 
             - name: Install Composer packages
               uses: ramsey/composer-install@v3

phpstan.neon
@@ -4,6 +4,6 @@ parameters:
     level: 8
     paths:
         - src
-    phpVersion: 80200
+    phpVersion: 70200
     errorFormat: ticketswap
     editorUrl: 'phpstorm://open?file=%%file%%&line=%%line%%'

commit 4eea209a3723c4dd1f7ef0f4f9aa2fa637714896

Fix broken link format detection
This broke in #10. The condition was wrong.

Added tests to assure this won't break again.

extension.neon
@@ -5,3 +5,4 @@ services:
             relativePathHelper: '@simpleRelativePathHelper'
             ciDetectedErrorFormatter: '@PHPStan\Command\ErrorFormatter\CiDetectedErrorFormatter'
             editorUrl: '%editorUrl%'
+            environmentVariables: '%env%'

src/TicketSwapErrorFormatter.php
@@ -27,31 +27,39 @@ final class TicketSwapErrorFormatter implements ErrorFormatter
     private ErrorFormatter $ciDetectedErrorFormatter;
     private ?string $editorUrl;
 
+    /**
+     * @param RelativePathHelper $relativePathHelper
+     * @param ErrorFormatter $ciDetectedErrorFormatter
+     * @param string|null $editorUrl
+     * @param array<string, mixed> $environmentVariables
+     */
     public function __construct(
         RelativePathHelper $relativePathHelper,
         ErrorFormatter $ciDetectedErrorFormatter,
-        ?string $editorUrl = null
+        ?string $editorUrl,
+        array $environmentVariables
     ) {
         $this->relativePathHelper = $relativePathHelper;
         $this->ciDetectedErrorFormatter = $ciDetectedErrorFormatter;
         $this->editorUrl = $editorUrl;
-        $this->linkFormat = self::getLinkFormatFromEnv();
+        $this->linkFormat = self::getLinkFormatFromEnv($environmentVariables);
     }
 
     /**
+     * @param array<string, mixed> $environmentVariables
      * @return self::LINK_FORMAT_*
      */
-    public static function getLinkFormatFromEnv() : string
+    public static function getLinkFormatFromEnv(array $environmentVariables) : string
     {
-        if (getenv('GITHUB_ACTIONS') !== false) {
+        if (isset($environmentVariables['GITHUB_ACTIONS'])) {
             return self::LINK_FORMAT_GITHUB_ACTIONS;
         }
 
-        if (getenv('TERMINAL_EMULATOR') !== 'JetBrains-JediTerm') {
+        if (isset($environmentVariables['TERMINAL_EMULATOR']) && $environmentVariables['TERMINAL_EMULATOR'] === 'JetBrains-JediTerm') {
             return self::LINK_FORMAT_PHPSTORM;
         }
 
-        if (getenv('TERM_PROGRAM') !== 'WarpTerminal') {
+        if (isset($environmentVariables['TERM_PROGRAM']) && $environmentVariables['TERM_PROGRAM'] === 'WarpTerminal') {
             return self::LINK_FORMAT_WARP;
         }
 

tests/TicketSwapErrorFormatterTest.php
@@ -31,7 +31,46 @@ final class TicketSwapErrorFormatterTest extends TestCase
                     return 0;
                 }
             },
-            self::PHPSTOR_EDITOR_URL
+            self::PHPSTOR_EDITOR_URL,
+            []
+        );
+    }
+
+    /**
+     * @return iterable<array{TicketSwapErrorFormatter::LINK_FORMAT_*, array<string, string>}>
+     */
+    public static function provideLinkFormatFromEnv() : iterable
+    {
+        yield 'GitHub Actions' => [
+            TicketSwapErrorFormatter::LINK_FORMAT_GITHUB_ACTIONS,
+            ['GITHUB_ACTIONS' => 'true'],
+        ];
+        yield 'JetBrains' => [
+            TicketSwapErrorFormatter::LINK_FORMAT_PHPSTORM,
+            ['TERMINAL_EMULATOR' => 'JetBrains-JediTerm'],
+        ];
+        yield 'Warp' => [
+            TicketSwapErrorFormatter::LINK_FORMAT_WARP,
+            ['TERM_PROGRAM' => 'WarpTerminal'],
+        ];
+        yield 'Ghostty' => [
+            TicketSwapErrorFormatter::LINK_FORMAT_DEFAULT,
+            ['TERM_PROGRAM' => 'ghostty'],
+        ];
+        yield 'Default' => [
+            TicketSwapErrorFormatter::LINK_FORMAT_DEFAULT,
+            [],
+        ];
+    }
+
+    /**
+     * @dataProvider provideLinkFormatFromEnv
+     */
+    public function testGetLinkFormatFromEnv(string $expected, array $environmentVariables) : void
+    {
+        self::assertSame(
+            $expected,
+            TicketSwapErrorFormatter::getLinkFormatFromEnv($environmentVariables)
         );
     }
 