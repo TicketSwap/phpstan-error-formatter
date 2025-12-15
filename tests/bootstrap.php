34efc30eeaed03823c091da783889bd74e8615b6
Abdul Malik Ikhsan <samsonasik@gmail.com>
2025-12-15 21:11:37 +0700

    Remove unnecessary custom PHPUnit bootstrap to load phpstan preload (#26)

--- tests/bootstrap.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-declare(strict_types=1);
-
-include __DIR__ . '/../vendor/autoload.php';
-
-Phar::loadPhar(__DIR__ . '/../vendor/phpstan/phpstan/phpstan.phar', 'phpstan.phar');
-
-require_once('phar://phpstan.phar/preload.php');
